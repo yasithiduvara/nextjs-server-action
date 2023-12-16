e56e1be8112a8b7ffaa05355654c031dee3e5a8f
yasith <yasithiduvara@gmail.com>
2023-12-16 12:16:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <main>
+    <h1 className=' text-center text-black text-[10vh]'>Server Action</h1>
+   </main>
+  )
+}
